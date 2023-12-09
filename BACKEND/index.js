ec1da7977512eac22b429f436b4cb34a95f4b4c1
Ian Mateos <matgoni19@gmail.com>
2023-12-09 12:32:38 -0600

    UPDATE(backend): Inicializazion y modularizacion de proyecto, prueba basica de servido

---
+++ BACKEND/index.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import dotenv from 'dotenv';
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ok: true});
+})
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log("🛰️ 🛰️   http://localhost:" + PORT));
